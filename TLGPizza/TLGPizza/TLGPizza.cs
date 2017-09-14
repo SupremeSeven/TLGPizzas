c54a263be292df12eb0554b0dd8a2239d06b66f2
Daniel Muldrew <dmuldrew@yahoo.com>
2017-09-14 11:45:21 -0700

    add Stores object

--- TLGPizza/TLGPizza/TLGPizza.cs
+++ TLGPizza/TLGPizza/TLGPizza.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace TLGPizza
 {
@@ -11,5 +13,42 @@ namespace TLGPizza
         static void Main(string[] args)
         {
         }
+    }
+
+    class Stores
+    {
+        Stores stores;
+        private decimal totalSales;
+
+
+        public Stores()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string StoreSales = @"SELECT s.Name, pd.Amount, p.PurchaseTotal
+                                        FROM TLGPizza.Store s
+                                        JOIN TLGPizza.Payment p
+                                        ON s.StoreId = p.StoreId
+                                        JOIN TLGPizza.PaymentDue pd
+                                        ON pd.PaymentDueId = p.PaymentDueId
+                                        ORDER BY s.Name;";
+
+                using (SqlCommand cmd = new SqlCommand(StoreSales, connection))
+                {
+                    connection.Open();
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        //   for (int)
+                    }
+                    connection.Close();
+                }
+            }
+
+        }
+    }
+
+    class Store
+    {
+
     }
 }
