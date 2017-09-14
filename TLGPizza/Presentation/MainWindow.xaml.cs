2fd06c46eee4e6c0e88f8ba7bc14dd79c8943d41
Quinn Liu <Quinnl@tlgclass.com>
2017-09-14 13:14:50 -0700

    connected to test database

--- TLGPizza/Presentation/MainWindow.xaml.cs
+++ TLGPizza/Presentation/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Configuration;
 
 namespace Presentation
 {
@@ -23,31 +23,38 @@ namespace Presentation
     /// </summary>
     public partial class MainWindow : Window
     {
+        string constring = ConfigurationManager.ConnectionStrings
+          ["Connection_String"].ConnectionString;
+
+        SqlConnection con;
         public MainWindow()
         {
             InitializeComponent();
+            con = new SqlConnection(constring);
 
             List<string> Store = new List<string> { "testStore1", "testStore2", "testStore3" };
             StoreList.ItemsSource = Store;
         }
 
-        //    string connectionString = @"Data Source=LOCALHOST\SQLEXPRESS; Initial Catalog=TestDatabase; Integrated Security=True;";
-        //    using (SqlConnection connection = new SqlConnection(connectionString))
-        //    {
-        //        string truncateTransactions = "TRUNCATE TABLE [Test].[Transactions];";
-        //        string truncateStores = "TRUNCATE TABLE [Test].[Stores];";
-
-        //        using (SqlCommand cmd = new SqlCommand(truncateTransactions, connection))
+        //    string connectionString = @"Data Source=LOCALHOST\SQLEXPRESS; 
+        //Initial Catalog=TestDatabase; Integrated Security=True;";
+        //        using (SqlConnection connection = 
+        //new SqlConnection(connectionString))
         //        {
-        //            connection.Open();
+        //            string truncateTransactions = "TRUNCATE TABLE [Test].[Transactions];";
+        //string truncateStores = "TRUNCATE TABLE [Test].[Stores];";
 
-        //            cmd.ExecuteNonQuery();
+        //            using (SqlCommand cmd = new SqlCommand(truncateTransactions, connection))
+        //            {
+        //                connection.Open();
 
-        //            cmd.CommandText = truncateStores;
-        //            cmd.ExecuteNonQuery();
+        //                cmd.ExecuteNonQuery();
+
+        //                cmd.CommandText = truncateStores;
+        //                cmd.ExecuteNonQuery();
+        //            }
         //        }
         //    }
-        //}
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
@@ -58,5 +65,13 @@ namespace Presentation
         {
             MessageBox.Show("foo");
         }
+
+        private void TestConnection_Click(object sender, RoutedEventArgs e)
+        {
+            con.Open();
+            MessageBox.Show("Connected");
+            con.Close();
+        }
+
     }
 }
